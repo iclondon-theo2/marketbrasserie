f95d744e23a5a65ceab6f4a0ebeca374f426b57c
Adam Szklenar <szkl.adam@adams-mbp.home>
2021-10-24 11:57:14 +0100

    pancake price, css, js corr

--- js/script.js
+++ js/script.js
@@ -5,46 +5,6 @@ gsap.from("h1", {duration: 3, opacity: 0, ease: "power2", stagger: 0.2, });
 
 function init(){
 	
-	gsap.to("#foodicon", {rotation: 30, scrollTrigger: {
-		trigger: "#food",
-		start: "top 7%",
-		end: "bottom bottom",
-		scrub: 2,
-		}
-	 }); 
-	
-	gsap.to("#teaicon", {rotation: 30, scrollTrigger: {
-		trigger: "#tea",
-		start: "top 7%",
-		end: "bottom bottom",
-		scrub: 2,
-		}
-	 }); 
-	
-	gsap.to("#champicon", {rotation: 30, scrollTrigger: {
-		trigger: "#champ",
-		start: "top 7%",
-		end: "bottom bottom",
-		scrub: 2,
-		}
-	 }); 
-	
-	gsap.to("#flowericon", {rotation: 30, scrollTrigger: {
-		trigger: "#story",
-		start: "top 7%",
-		end: "bottom bottom",
-		scrub: 2,
-		}
-	 }); 
-	
-	gsap.to("#bookicon", {rotation: 30, scrollTrigger: {
-		trigger: ".book",
-		start: "top 7%",
-		end: "bottom bottom",
-		scrub: 2,
-		}
-	 });
-	
 	gsap.from(".icon", {opacity: 0, duration: 2, scrollTrigger: {
 		trigger: ".icon",
 		start: "top bottom",
@@ -52,20 +12,6 @@ function init(){
 		scrub: 1,
 		}
 	 });
-	
-	const flower = gsap.utils.toArray(".flower");
-	flower.forEach(flower => {
-		gsap.to(flower, {
-			rotation: 360, 
-			transformOrigin: "50% 50%", 
-			scrollTrigger: {
-				trigger: "section",
-				start: "top bottom",
-				end: "bottom bottom",
-				scrub: 2,
-			}
-		})
-	});
 	
 	const h3 = gsap.utils.toArray("h3");
 	h3.forEach(h3 => {
